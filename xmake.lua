9582f6422554ce9873640a1ca4f5c9076e4a6228
yangjz1125 <yangjz1125@outlook.com>
2018-12-16 22:58:46 +0800

    Update for C language.

--- xmake.lua
+++ xmake.lua
@@ -2,9 +2,12 @@ target("ldev")
 	set_kind("static")
 	add_headers("(ldev/*.h)")
 	add_files("ldev/*.c")
+	set_languages("c99")
 
 target("ldev_test")
 	set_kind("binary")
 	add_files("test/*.c")
 	add_deps("ldev")
+	set_languages("c99")
+	
 	
